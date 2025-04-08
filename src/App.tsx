2bab691b78e122544d060cca4f2c56258a515eed
Davidlevy13 <levy.david1993@gmail.com>
2025-04-08 15:22:06 +0200

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,28 @@ import { useState } from 'react'
 import EmployeeCard from './components/EmployeeCard'
 import './App.css'
 
+const sampleEmployee = {
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  email: "charlie.thompson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
+
 function App() {
-  const [employee, setEmployee] = useState(employee);
+  const [employee, setEmployee] = useState(sampleEmployee);
   const getEmployee = () => {
 
     fetch("https://randomuser.me/api?nat=en")
       .then((response) => response.json())
       .then((data) => {
-        setEmployee(data.results[0]);
+        ;setEmployee(data.results[0]);
       });
   };
+
   return (
     <div className='App'>
       <EmployeeCard employee={employee} />
@@ -19,5 +31,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App
